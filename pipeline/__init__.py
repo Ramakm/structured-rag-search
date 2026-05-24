f2dcf5f5b1a7436fb3fae68cdbbe1f54025f1bae
Ramakm <itsramakrushna@gmail.com>
2026-05-24 23:08:45 +0530

    updated

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -1,6 +1,6 @@
 from .ingestion import Document, Section, Chunk, load_documents
 from .chunker import TokenChunker
-from .embedder import Embedder
+from .embedder import embed_texts, embed_query
 from .indexer import Indexer
 
-__all__ = ["Document", "Section", "Chunk", "load_documents", "TokenChunker", "Embedder", "Indexer"]
+__all__ = ["Document", "Section", "Chunk", "load_documents", "TokenChunker", "embed_texts", "embed_query", "Indexer"]
